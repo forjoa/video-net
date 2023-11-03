7b74a4ba5ecba3ca476ccfa6c1ddf6e791e66b48
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-11-04 00:47:50 +0100

    Login page without styles

--- frontend/components/Login.jsx
+++ frontend/components/Login.jsx
@@ -1,10 +1,18 @@
+// styles
+import '../styles/Login.css'
 
 const Login = () => {
     return (
         <div className="login-form-container">
+            <h1>Login </h1>
             <form>
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" />
+                <input type="email" name="email" id="email" autoComplete="email"/>
+
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="password" />
+
+                <input type="submit" value="Login" />
             </form>
         </div>
     )
