09cd901e4813ebc77cdb9d7e72f5a8c0bd496120
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-11-07 10:28:16 +0100

    Register login without uploading profile photo

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,58 +1,110 @@
-import express from 'express'
-import database from '../database/database.js'
-import bcrypt from 'bcrypt' 
+import express from "express";
+import bcrypt from "bcrypt";
+import multer from "multer";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import database from "../database/database.js";
 
-const user = express.Router()
+const user = express.Router();
 
-user.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    const query = 'SELECT * FROM users WHERE email = ?';
-    
-    database.query(query, [email], async (err, results) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json(err);
-            return;
-        }
+// multer config
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-        // user doesn't exist
-        if (results.length === 0) {
-            res.status(401).json({ error: 'User not found' });
-            return;
-        }
-
-        const user = results[0];
-        const hashedPassword = user.pwd;
-
-        try {
-            const passwordMatch = await bcrypt.compare(password, hashedPassword);
-            
-            if (passwordMatch) {
-                res.status(200).json({ message: 'Autenticación exitosa', id: user.id, username: user.username });
-            } else {
-                // incorrect password
-                res.status(401).json({ error: 'Incorrect password' });
-            }
-        } catch (error) {
-            console.error(error);
-            res.status(500).json(error);
-        }
-    });
+const uploadProfileImage = multer({
+  storage: multer.diskStorage({
+    destination: path.join(__dirname, "..", "..", "frontend", "assets", "uploads"),
+    filename: (req, file, cb) => {
+      cb(null, `${file.originalname}.${file.mimetype}`);
+    },
+  })
 });
 
 
-user.get('/users', (req, res) => {
-    const query = 'SELECT * FROM users'
+// login
+user.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const query = "SELECT * FROM users WHERE email = ?";
 
-    database.query(query, (err, result) => {
-        if (err) {
-            console.error(err)
-            res.status(500).json(err)
-            return
-        }
+  database.query(query, [email], async (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json(err);
+      return;
+    }
 
-        res.json(result)
-    })
-})
+    // user doesn't exist
+    if (results.length === 0) {
+      res.status(401).json({ error: "User not found" });
+      return;
+    }
 
-export default user
+    const user = results[0];
+    const hashedPassword = user.pwd;
+
+    try {
+      const passwordMatch = await bcrypt.compare(password, hashedPassword);
+
+      if (passwordMatch) {
+        res.status(200).json({
+          message: "Authentication correctly",
+          id: user.id,
+          username: user.username,
+        });
+      } else {
+        // incorrect password
+        res.status(401).json({ error: "Incorrect password" });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json(error);
+    }
+  });
+});
+
+// register
+user.post(
+  "/register",
+  uploadProfileImage.single("profileImage"),
+  async (req, res) => {
+    const data = req.body;
+    const photoPath = await uploadProfileImage.single("profileImage").result;
+    const query =
+      "INSERT INTO users(username, description, photo, email, pwd) VALUES(?,?,?,?,?)";
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+    const values = [
+      data.username,
+      data.description,
+      photoPath,
+      data.email,
+      hashedPassword,
+    ];
+
+    database.query(query, values, (err, result) => {
+      if (err) {
+        res.status(500).send({ message: `Error while saving data ${result}` });
+        return;
+      }
+
+      res.status(200).send({ message: "Data saved well" });
+    });
+  }
+);
+
+// get all users
+user.get("/users", (req, res) => {
+  const query = "SELECT * FROM users";
+
+  database.query(query, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json(err);
+      return;
+    }
+
+    res.json(result);
+  });
+});
+
+export default user;
--- frontend/components/Register.jsx
+++ frontend/components/Register.jsx
@@ -7,18 +7,63 @@ import '../styles/Register.css'
 // icons
 import lockedIcon from '../assets/icons/lock.svg'
 import unlockedIcon from '../assets/icons/unlock.svg'
+import { useNavigate } from "react-router"
 
 const Register = () => {
     const [locked, setLocked] = useState(true)
+    const navigate = useNavigate()
+    // form values
+    const [username, setUsername] = useState('')
+    const [description, setDescription] = useState('')
+    const [photo, setPhoto] = useState(null);
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
     useEffect(() => { document.title = 'Video Net | Register' }, [])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const data = {
+            username: username,
+            description: description,
+            photo: photo,
+            email: email,
+            password: password
+        }
+
+        try {
+            const response = await fetch('http://localhost:3000/api/user/register', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            });
+
+            const res = await response.json()
+            if (response.ok) {
+                navigate('/');
+            } else {
+                alert(res.error);
+            }
+        } catch (err) {
+            console.error(`Error communication: ${err}`)
+        }
+    }
+
+    const handlePhotoChange = (e) => {
+        document.querySelector('#photo-name-file').style.display = 'block'
+        document.querySelector('#photo-name-file').src = URL.createObjectURL(e.target.files[0])
+        document.querySelector('#label-photo').style.display = 'none'
+
+        setPhoto(e.target.files[0])
+    }
 
     return (
         <>
             <div className="circle"></div>
             <div className="login-form-container">
                 <h1>Register</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <label htmlFor="username">Username</label>
                     <input
                         type="text"
@@ -26,6 +71,8 @@ const Register = () => {
                         id="username"
                         className='username-input'
                         autoComplete="off"
+                        value={username}
+                        onChange={(e) => { setUsername(e.target.value) }}
                     />
 
                     <label htmlFor="description">Description</label>
@@ -33,13 +80,19 @@ const Register = () => {
                         name="description"
                         id="description"
                         className="description-input"
+                        value={description}
+                        onChange={(e) => { setDescription(e.target.value) }}
                     />
 
-                    <label htmlFor="photo">Photo</label>
-                    <input 
-                        type="file" 
-                        name="photo"
-                        id="photo"
+                    <p className='choose-photo'>Profile photo</p>
+                    <label htmlFor='photo' className='label-photo' id='label-photo'>Foto de perfil </label>
+                    <img id='photo-name-file' alt='Profile photo' style={{ display: 'none' }} />
+                    <input
+                        type='file'
+                        className='photo'
+                        name='photo'
+                        id='photo'
+                        onChange={(e) => handlePhotoChange(e)}
                     />
 
                     <label htmlFor="email">Email</label>
@@ -49,6 +102,8 @@ const Register = () => {
                         id="email"
                         className='email-input'
                         autoComplete="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
 
                     <label htmlFor="password">Password</label>
@@ -58,6 +113,8 @@ const Register = () => {
                             name="password"
                             id="password"
                             className='password-input'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
                         <img
                             src={locked ? lockedIcon : unlockedIcon}
