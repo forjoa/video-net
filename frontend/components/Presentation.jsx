a3389d9e5a939ade2b45801ac7d321f322448f39
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-12-10 10:46:16 +0100

    Presentation Header finished

--- frontend/components/Presentation.jsx
+++ frontend/components/Presentation.jsx
@@ -4,7 +4,14 @@ import '../styles/Presentation.css'
 // components 
 import PresentationHeader from './presentation/PresentationHeader.jsx'
 
+// imports 
+import { useEffect } from 'react'
+
 const Presentation = () => {
+    useEffect(() => {
+        document.title = 'Video Net | Presentation'
+    })
+
     return (
         <>
             <PresentationHeader />
--- frontend/components/presentation/PresentationHeader.jsx
+++ frontend/components/presentation/PresentationHeader.jsx
@@ -1,11 +1,24 @@
 // icons 
 import videoLogo from '../../assets/icons/videoLogo.png'
 
+// imports 
+import { Link } from 'react-router-dom'
+
 const PresentationHeader = () => {
     return (
         <header className='presentation-header'>
-            <div className="logo-container">
-                <img src={videoLogo} alt="Video Logo" />
+            <div className="presentation-header-content">
+                <div className="logo-container">
+                    <Link to='/presentation'>
+                        <img src={videoLogo} alt="Video Logo" />
+                        <p>Video Net </p>
+                    </Link>
+                </div>
+
+                <div className="login-container">
+                    <Link to='/login' className='login-button'>Login</Link>
+                    <Link to='/register' className='register-button'>Register</Link>
+                </div>
             </div>
         </header>
     )
