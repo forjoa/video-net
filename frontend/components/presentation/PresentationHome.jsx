89c9eaa873aeee8371e4c47c9be2aa83a24e6e1b
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-12-14 17:53:45 +0100

    Added presentation title without image

--- frontend/components/presentation/PresentationHome.jsx
+++ frontend/components/presentation/PresentationHome.jsx
@@ -2,6 +2,18 @@
 const PresentationHome = () => {
     return (
         <>
+            <div className="presentation-home-container">
+                <div className="presentation-home-content">
+                    <div className="presentation-home-title">
+                        <h1>
+                            Video Net, where you can share every video important for you
+                        </h1>
+                    </div>
+                    <div className="presentation-home-image">
+                        
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
