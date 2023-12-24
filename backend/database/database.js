4857f3256f42c4853b6e33aa22f58f06270a2113
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-12-24 18:18:21 +0100

    Added and updated env variables

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,10 +1,21 @@
 import mysql from 'mysql2'
+import pkg from 'pg'
+
+const { Pool } = pkg
 
 const database = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'videonet'
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'videonet',
 })
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + '?sslmode=require',
+})
+
+if (pool) {
+  console.log('Postgre connection made correctly')
+}
 
 export default database
